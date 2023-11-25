ff35d7d5e048b7432fe7a840df3a9bc0f00eb52a
Omnath Mandal <145841395+htanmo@users.noreply.github.com>
2023-11-25 14:00:56 +0530

    fixed: out of bound error

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,16 @@ use tpad::{Task, HELP};
 fn main() {
     let mut task = Task::new();
     let args: Vec<String> = env::args().collect();
-    let command = args[1].as_str();
     match args.len() {
         1 => task.list(),
-        2 => match command {
+        2 => match args[1].as_str() {
             "init" => task.init(),
             "list" => task.list(),
             "reset" => task.reset(),
             "restore" => task.restore(),
             "help" | "--help" | "-h" | _ => println!("{}", HELP),
         },
-        _ => match command {
+        _ => match args[1].as_str() {
             "add" => task.add(&args[2..]),
             "done" => task.done(&args[2..]),
             "undo" => task.undo(&args[2..]),
